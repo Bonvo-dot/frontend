48f243054c98001a458c22028c5ea8fc9d4fc568
mberguer <martin.berguer@gmail.com>
2022-10-31 01:16:28 -0300

    about us

--- src/components/home-v3.js
+++ src/components/home-v3.js
@@ -2,13 +2,7 @@ import React from 'react';
 import Navbar from './global-components/navbar-v3';
 import Banner from './section-components/banner-v3';
 import SearchForm from './section-components/search-form';
-import ProductSlider from './section-components/product-slider-v3';
 import ProductListing from './section-components/product-listing';
-import FeaturedItem from './section-components/featured-item-v1';
-import Video from './section-components/video-v1';
-import Testimonial from './section-components/testimonial-v3';
-import Sponsor from './section-components/sponsor-v1';
-import BlogSlider from './blog-components/blog-slider-v1';
 import CallToActionV1 from './section-components/call-to-action-v1';
 import Footer from './global-components/footer';
 
@@ -17,13 +11,7 @@ const Home_V3 = () => {
         <Navbar />
         <Banner />
         <SearchForm />
-        {/* <ProductSlider /> */}
         <ProductListing />
-        <FeaturedItem />
-        <Video />
-        <Testimonial />
-        <Sponsor />
-        <BlogSlider sectionClass="pt-120"/>
         <CallToActionV1 />
         <Footer />
     </div>
